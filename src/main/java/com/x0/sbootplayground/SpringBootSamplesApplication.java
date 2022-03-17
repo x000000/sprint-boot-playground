48d7cb849aa9e0e481070f628ad52406538f344f
x000000 <o.o.n.joy@gmail.com>
2022-03-17 16:16:14 +0300

    Add app skeleton

---
+++ src/main/java/com/x0/sbootplayground/SpringBootSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.x0.sbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/x0/sbootplayground/SpringBootSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x0.sbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
