ff8651c78efba64317a58c25c6d5e57db7f38e77
x000000 <o.o.n.joy@gmail.com>
2022-04-07 01:42:38 +0300

    Add docker config

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,21 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const srcDir = process.env.VUE_SRC || './src/main/resources/assets';
+const outDir = process.env.VUE_OUT || '../public';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src/main/resources/assets', import.meta.url))
+      '@': fileURLToPath(new URL(srcDir, import.meta.url))
     }
   },
   build: {
-    outDir: '../public',
+    outDir: outDir,
   },
-  root: './src/main/resources/assets',
+  root: srcDir,
   server: {
     proxy: {
       '/api': {
